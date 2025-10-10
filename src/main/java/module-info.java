0ee54905c737df9cb36cb32441e839eef97d0c05
Programação com Java e C sharp <marcelomdsmk@gmail.com>
2025-10-10 20:59:18 -0300

    Refatoração do projeto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.mmds.crud_usuarios_java {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+    requires java.dotenv;
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.mmds.crud_usuarios_java to javafx.fxml;
+    opens com.mmds.crud_usuarios_java.controller to javafx.fxml;
+    opens com.mmds.crud_usuarios_java.model to javafx.base;
+
+    exports com.mmds.crud_usuarios_java;
+}
